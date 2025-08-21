fix: multihead attention computation was performed incorrectly, resolved. Also added causal masking

fm/nn/attention.py
@@ -3,18 +3,31 @@ import jax.numpy as jnp
 import flax.linen as nn
 
 
+def attention(q, k, v, d, causal):
+    q = jnp.transpose(q, (0, 2, 1, 3))
+    k = jnp.transpose(k, (0, 2, 3, 1))
+    v = jnp.transpose(v, (0, 2, 1, 3))
+    scores = (q @ k) / jnp.sqrt(d)
+    if causal:
+        mask = jnp.tril(scores)
+        big_neg = jnp.finfo(jnp.float32).min
+        scores = jnp.where(mask == 0, big_neg, scores)
+
+    out = nn.softmax(scores, axis=-1) @ v
+    out = out.transpose(0, 2, 1, 3)
+    return out
+
+
 class MultiheadAttention(nn.Module):
     emb_dim: int
     n_heads: int
     @nn.compact
-    def __call__(self, x):
+    def __call__(self, x, causal: bool = True):
         qkv_dim = self.emb_dim // self.n_heads
-        q = nn.DenseGeneral(features=(qkv_dim, self.n_heads))(x)
-        k = nn.DenseGeneral(features=(qkv_dim, self.n_heads))(x)
-        v = nn.DenseGeneral(features=(qkv_dim, self.n_heads))(x)
-        k_t = jnp.swapaxes(k, -1, -2)
-        scores = (q @ k_t) / jnp.sqrt(qkv_dim)
-        out = nn.softmax(scores, axis=-1) @ v
+        q = nn.DenseGeneral(features=(self.n_heads, qkv_dim))(x)
+        k = nn.DenseGeneral(features=(self.n_heads, qkv_dim))(x)
+        v = nn.DenseGeneral(features=(self.n_heads, qkv_dim))(x)
+        out = attention(q, k, v, qkv_dim, causal)
         return out.reshape(x.shape[:-1] + (self.emb_dim,))
     
 
@@ -22,16 +35,12 @@ class MultiQueryAttention(nn.Module):
     emb_dim: int
     n_heads: int
     @nn.compact
-    def __call__(self, x):
+    def __call__(self, x, causal: bool = True):
         qkv_dim = self.emb_dim // self.n_heads
-        q = nn.DenseGeneral(features=(qkv_dim, self.n_heads))(x)
-        k = nn.DenseGeneral(features=(qkv_dim, 1))(x)
-        k = jnp.repeat(k, self.n_heads, axis=-1)
-        v = nn.DenseGeneral(features=(qkv_dim, 1))(x)
-        v = jnp.repeat(v, self.n_heads, axis=-1)
-        k_t = jnp.swapaxes(k, -1, -2)
-        scores = (q @ k_t) / jnp.sqrt(qkv_dim)
-        out = nn.softmax(scores, axis=-1) @ v
+        q = nn.DenseGeneral(features=(self.n_heads, qkv_dim))(x)
+        k = nn.DenseGeneral(features=(1, qkv_dim))(x).repeat(self.n_heads, axis=-2)
+        v = nn.DenseGeneral(features=(1, qkv_dim))(x).repeat(self.n_heads, axis=-2)
+        out = attention(q, k, v, qkv_dim, causal)
         return out.reshape(x.shape[:-1] + (self.emb_dim,))
 
 
@@ -40,14 +49,10 @@ class GroupedQueryAttention(nn.Module):
     n_heads: int
     scale: int
     @nn.compact
-    def __call__(self, x):
+    def __call__(self, x, causal: bool = True):
         qkv_dim = self.emb_dim // self.n_heads
-        q = nn.DenseGeneral(features=(qkv_dim, self.n_heads))(x)
-        k = nn.DenseGeneral(features=(qkv_dim, self.n_heads // self.scale))(x)
-        k = jnp.repeat(k, self.scale, axis=-1)
-        v = nn.DenseGeneral(features=(qkv_dim, self.n_heads // self.scale))(x)
-        v = jnp.repeat(v, self.scale, axis=-1)
-        k_t = jnp.swapaxes(k, -1, -2)
-        scores = (q @ k_t) / jnp.sqrt(qkv_dim)
-        out = nn.softmax(scores, axis=-1) @ v
+        q = nn.DenseGeneral(features=(self.n_heads, qkv_dim))(x)
+        k = nn.DenseGeneral(features=(self.n_heads // self.scale, qkv_dim))(x).repeat(self.scale, axis=-2)
+        v = nn.DenseGeneral(features=(self.n_heads // self.scale, qkv_dim))(x).repeat(self.scale, axis=-2)
+        out = attention(q, k, v, qkv_dim, causal)
         return out.reshape(x.shape[:-1] + (self.emb_dim,))